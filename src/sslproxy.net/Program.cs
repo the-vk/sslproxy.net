06fb309e5e78110baa362c3b282819d63bfdf927
Evgeny Pavlov <jdiablo@mail.ru>
2015-05-05 11:46:01 +0300

    Spaces replaced with tabs

--- src/sslproxy.net/Program.cs
+++ src/sslproxy.net/Program.cs
@@ -55,7 +55,7 @@ namespace sslproxy.net
 			if (options.Console)
 			{
 				Log.Info("Running in console mode.");
-                Log.Info("Options:" + options.ToString());
+				Log.Info("Options:" + options.ToString());
 				proxyEngine.Start();
 				Console.WriteLine("Press enter to exit...");
 				Console.ReadLine();
@@ -64,7 +64,7 @@ namespace sslproxy.net
 			else
 			{
 				Log.Info("Running in service mode.");
-                Log.Info("Options:" + options.ToString());
+				Log.Info("Options:" + options.ToString());
 				var services = new ServiceBase[] {new ProxyEngineService(proxyEngine)};
 				ServiceBase.Run(services);
 			}
